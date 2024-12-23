6ff93855fcc47c00c785320181e56145fa0c8f00
Tolu Awosika <etolulope64@gmail.com>
2024-12-23 21:14:23 +0100

    additional commit

--- src/components/EmptyResults.jsx
+++ src/components/EmptyResults.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import sledge from '../assets/images/illustration-empty.svg'
 
 
 function EmptyResults() {
   return (
     <div id="empty-results-text" className="empty-results-text">
-          <img src="./src/assets/images/illustration-empty.svg" alt=""/>
+          <img src={sledge} alt=""/>
           <h2>Results shown here</h2>
           <p className="h2-text"> Complete the form and click “calculate repayments” to see what 
             your monthly repayments would be.</p>
